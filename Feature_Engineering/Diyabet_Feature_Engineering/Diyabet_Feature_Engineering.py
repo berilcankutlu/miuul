7a371f3efe83c21cf52227f60ee18ea1bbdc538b
Berilcan Kutlu <42407842+berilcankutlu@users.noreply.github.com>
2024-09-23 19:40:23 +0300

    Update Diyabet_Feature_Engineering.py

--- Feature_Engineering/Diyabet_Feature_Engineering/Diyabet_Feature_Engineering.py
+++ Feature_Engineering/Diyabet_Feature_Engineering/Diyabet_Feature_Engineering.py
@@ -1,21 +1,5 @@
 ################################################
 # Diabete Feature Engineering
-# Bu projede özellikleri verilen insanların diyabet hastası olup olamayacağını tahmin eden bir makine öğrenmesi modeli kurulmuştur.
-
-# Veri seti ABD'deki Ulusal Diyabet-Sindirim-Böbrek Hastalıkları Enstitüleri'nde tutulan büyük veri setinin parçasıdır. ABD'deki
-# Arizona Eyaleti'nin en büyük 5. şehri olan Phoenix şehrinde yaşayan 21 yaş ve üzerinde olan Pima Indian kadınları üzerinde
-# yapılan diyabet araştırması için kullanılan verilerdir.
-# Hedef değişken "outcome" olarak belirtilmiş olup; 1 diyabet test sonucunun pozitif oluşunu, 0 ise negatif oluşunu belirtmektedir.
-
-# Pregnancies              : Hamilelik sayısı
-# Glucose                  : Oral glikoz tolerans testinde 2 saatlik plazma glikoz konsantrasyonu
-# Blood Pressure           : Kan Basıncı (Küçük tansiyon) (mm Hg)
-# SkinThickness            : Cilt Kalınlığı
-# Insulin                  : 2 saatlik serum insülini (mu U/ml)
-# DiabetesPedigreeFunction : Fonksiyon (Oral glikoz tolerans testinde 2 saatlik plazma glikoz konsantrasyonu)
-# BMI                      : Vücut kitle endeksi
-# Age                      : Yaş (yıl)
-# Outcome                  : Hastalığa sahip (1) ya da değil (0)
 
 import numpy as np
 import pandas as pd

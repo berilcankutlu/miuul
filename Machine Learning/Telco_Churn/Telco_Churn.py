4b7fcf95a654af95140af24fa9bab901572e2051
Berilcan Kutlu <42407842+berilcankutlu@users.noreply.github.com>
2024-09-23 21:06:24 +0300

    Update Telco_Churn.py

--- Machine Learning/Telco_Churn/Telco_Churn.py
+++ Machine Learning/Telco_Churn/Telco_Churn.py
@@ -1,28 +1,5 @@
 ############################################################
 # Telco Customer
-
-# CustomerId       : Müşteri İd’si
-# Gender           : Cinsiyet
-# SeniorCitizen    : Müşterinin yaşlı olup olmadığı (1, 0)
-# Partner          : Müşterinin bir ortağı olup olmadığı (Evet, Hayır) ? Evli olup olmama
-# Dependents       : Müşterinin bakmakla yükümlü olduğu kişiler olup olmadığı (Evet, Hayır) (Çocuk, anne, baba, büyükanne)
-# tenure           : Müşterinin şirkette kaldığı ay sayısı
-# PhoneService     : Müşterinin telefon hizmeti olup olmadığı (Evet, Hayır)
-# MultipleLines    : Müşterinin birden fazla hattı olup olmadığı (Evet, Hayır, Telefon hizmeti yok)
-# InternetService  : Müşterinin internet servis sağlayıcısı (DSL, Fiber optik, Hayır)
-# OnlineSecurity   : Müşterinin çevrimiçi güvenliğinin olup olmadığı (Evet, Hayır, İnternet hizmeti yok)
-# OnlineBackup     : Müşterinin online yedeğinin olup olmadığı (Evet, Hayır, İnternet hizmeti yok)
-# DeviceProtection : Müşterinin cihaz korumasına sahip olup olmadığı (Evet, Hayır, İnternet hizmeti yok)
-# TechSupport      : Müşterinin teknik destek alıp almadığı (Evet, Hayır, İnternet hizmeti yok)
-# StreamingTV      : Müşterinin TV yayını olup olmadığı (Evet, Hayır, İnternet hizmeti yok) Müşterinin, bir üçüncü taraf sağlayıcıdan televizyon programları yayınlamak için İnternet hizmetini kullanıp kullanmadığını gösterir
-# StreamingMovies  : Müşterinin film akışı olup olmadığı (Evet, Hayır, İnternet hizmeti yok) Müşterinin bir üçüncü taraf sağlayıcıdan film akışı yapmak için İnternet hizmetini kullanıp kullanmadığını gösterir
-# Contract         : Müşterinin sözleşme süresi (Aydan aya, Bir yıl, İki yıl)
-# PaperlessBilling : Müşterinin kağıtsız faturası olup olmadığı (Evet, Hayır)
-# PaymentMethod    : Müşterinin ödeme yöntemi (Elektronik çek, Posta çeki, Banka havalesi (otomatik), Kredi kartı (otomatik))
-# MonthlyCharges   : Müşteriden aylık olarak tahsil edilen tutar
-# TotalCharges     : Müşteriden tahsil edilen toplam tutar
-# Churn            : Müşterinin kullanıp kullanmadığı (Evet veya Hayır) - Geçen ay veya çeyreklik içerisinde ayrılan müşteriler
-
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt

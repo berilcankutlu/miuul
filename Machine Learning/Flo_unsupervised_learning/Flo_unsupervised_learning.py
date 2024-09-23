211279921a5745963f572fa8424eba616681f693
Berilcan Kutlu <42407842+berilcankutlu@users.noreply.github.com>
2024-09-23 21:03:46 +0300

    Update Flo_unsupervised_learning.py

--- Machine Learning/Flo_unsupervised_learning/Flo_unsupervised_learning.py
+++ Machine Learning/Flo_unsupervised_learning/Flo_unsupervised_learning.py
@@ -1,24 +1,6 @@
 ###################################################################################
-
 # FLO Gözetimsiz Öğrenme ile Müşteri Segmentasyonu
 # FLO Customer Segmentation with Unsupervised Learning
-
-# iş problemi
-# FLO müşterilerini segmentlere ayırıp bu segmentlere göre pazarlama stratejileri belirlemek istiyor. Buna yönelik
-# olarak müşterilerin davranışları tanımlanacak ve bu davranışlardaki öbeklenmelere göre gruplar oluşturulacak.
-
-# master_id                         : Eşsiz müşteri numarası
-# order_channel                     : Alışveriş yapılan platforma ait hangi kanalın kullanıldığı (Android, ios, Desktop, Mobile)
-# last_order_channel                : En son alışverişin yapıldığı kanal
-# first_order_date                  : Müşterinin yaptığı ilk alışveriş tarihi
-# last_order_date                   : Müşterinin yaptığı son alışveriş tarihi
-# last_order_date_online            : Müşterinin online platformda yaptığı son alışveriş tarihi
-# last_order_date_offline           : Müşterinin offline platformda yaptığı son alışveriş tarihi
-# order_num_total_ever_online       : Müşterinin online platformda yaptığı toplam alışveriş sayısı
-# order_num_total_ever_offline      : Müşterinin offline'da yaptığı toplam alışveriş sayısı
-# customer_value_total_ever_offline : Müşterinin offline alışverişlerinde ödediği toplam ücret
-# customer_value_total_ever_online  : Müşterinin online alışverişlerinde ödediği toplam ücret
-# interested_in_categories_12       : Müşterinin son 12 ayda alışveriş yaptığı kategorilerin listesi
 
 # Görev 1: Veriyi Hazırlama
 # Adım1: flo_data_20K.csv verisini okutunuz.
